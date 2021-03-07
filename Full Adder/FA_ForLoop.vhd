d3ae22b3bdc6e1f48599a228bd374f773f94e102
Shruti Murarka <shrutimurarka5@gmail.com>
2021-03-07 13:46:48 +0530

    Update FA_ForLoop.vhd

--- Full Adder/FA_ForLoop.vhd
+++ Full Adder/FA_ForLoop.vhd
@@ -6,13 +6,12 @@ use IEEE.numeric_std.all;
 entity FA_ForLoop is
 port(
   a, b, c: in  std_logic_vector(1 downto 0);
-  sum, carry: out std_logic_vector(1 downto 0)
-  );
+  sum, carry: out std_logic_vector(1 downto 0));
 end  FA_ForLoop;
 architecture fl1 of  FA_ForLoop is
 begin
-gen1: for i in 0 to 1 generate
-sum(i)<= a(i) xor b(i) xor c(i);
-carry(i)<=(a(i) and b(i)) or (b(i) and c(i)) or (c(i) and a(i));
-end generate gen1;
+    gen1: for i in 0 to 1 generate
+        sum(i)<= a(i) xor b(i) xor c(i);
+        carry(i)<=(a(i) and b(i)) or (b(i) and c(i)) or (c(i) and a(i));
+    end generate gen1;
 end fl1;
