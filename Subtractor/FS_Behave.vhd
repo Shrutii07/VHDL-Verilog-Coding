03f65c78b0bd18be60b7bd11b632087184a8037f
Shruti Murarka <shrutimurarka5@gmail.com>
2021-03-08 02:39:43 +0530

    Update and rename F_SUBTRACTOR_BEHAVIORAL.vhd to FS_Behave.vhd

--- Subtractor/FS_Behave.vhd
+++ Subtractor/FS_Behave.vhd
@@ -3,14 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-
-entity F_SUBTRACTOR_BEHAVIORAL is
-
+entity FS_Behave is
 Port ( A : in  STD_LOGIC_VECTOR (2 downto 0);
       Y : out  STD_LOGIC_VECTOR (1 downto 0));
-end FULLSUBTRACTOR_BEHAVIORAL;
+end FS_Behave;
 
-architecture Behavioral of F_SUBTRACTOR_BEHAVIORAL is
+architecture Behavioral of FS_Behave is
 begin
 process (A)
 begin
