1461afc7915d17e70ad9e48d913d86a7898186b0
Shruti Murarka <shrutimurarka5@gmail.com>
2021-03-08 02:47:55 +0530

    Update comparator2_tb.vhd

--- Comparator/comparator2_tb.vhd
+++ Comparator/comparator2_tb.vhd
@@ -5,58 +5,48 @@ ENTITY tb_comparator_VHDL IS
 END tb_comparator_VHDL;
  
 ARCHITECTURE behavior OF tb_comparator_VHDL IS 
- 
-    -- Component Declaration for the comparator in VHDL
- 
-    COMPONENT comparator2
+ COMPONENT comparator2
     PORT(
          A : IN  std_logic_vector(1 downto 0);
          B : IN  std_logic_vector(1 downto 0);
          A_less_B : OUT  std_logic;
          A_equal_B : OUT  std_logic;
-         A_greater_B : OUT  std_logic
-        );
-    END COMPONENT;
-   --Inputs
+         A_greater_B : OUT  std_logic);
+  END COMPONENT;
+   
    signal A : std_logic_vector(1 downto 0) := (others => '0');
    signal B : std_logic_vector(1 downto 0) := (others => '0');
-  --Outputs
+  
    signal A_less_B : std_logic;
    signal A_equal_B : std_logic;
    signal A_greater_B : std_logic;
 BEGIN
- 
- -- Instantiate the comparator in VHDL
-   uut: comparator2 PORT MAP (
+ uut: comparator2 PORT MAP (
           A => A,
           B => B,
           A_less_B => A_less_B,
           A_equal_B => A_equal_B,
-          A_greater_B => A_greater_B
-        );
-
-   -- Stimulus process
+          A_greater_B => A_greater_B);
+  
    stim_proc: process
    begin 
   -- create test cases for A_less_B
-  for i in 0 to 3 loop 
+  alb: for i in 0 to 3 loop 
            A <= std_logic_vector(to_unsigned(i,2));  
            B <= std_logic_vector(to_unsigned(i+1,2));  
            wait for 20 ns; 
-      end loop;
+      end loop alb;
   -- create test cases for A_greater_B
-   for i in 0 to 3 loop 
+   agb: for i in 0 to 3 loop 
            A <= std_logic_vector(to_unsigned(i+1,2));  
            B <= std_logic_vector(to_unsigned(i,2));  
            wait for 20 ns; 
-      end loop;
+      end loop agb;
   -- create test cases for A_equal_B
-  for i in 0 to 3 loop 
+  aeb: for i in 0 to 3 loop 
            A <= std_logic_vector(to_unsigned(i,2));  
            B <= std_logic_vector(to_unsigned(i,2));   
            wait for 20 ns; 
-      end loop;
-      wait;
+      end loop aeb;
    end process;
-
-END behavior;
+end behavior;
