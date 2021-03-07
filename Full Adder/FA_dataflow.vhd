549304e65b7a7828b798a9f9a6b7934f4f8ba744
Shruti Murarka <shrutimurarka5@gmail.com>
2021-03-07 13:49:09 +0530

    Update FA_dataflow.vhd

--- Full Adder/FA_dataflow.vhd
+++ Full Adder/FA_dataflow.vhd
@@ -1,16 +1,18 @@
 library IEEE; 
 use IEEE.STD_LOGIC_1164.ALL; 
 use IEEE.STD_LOGIC_ARITH.ALL; 
-use IEEE.STD_LOGIC_UNSIGNED.ALL; 
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 entity FA_dataflow is 
-Port ( a : in STD_LOGIC; 
-b : in STD_LOGIC; 
-cin : in STD_LOGIC; 
-s : out STD_LOGIC; 
-cout : out STD_LOGIC); 
+Port ( 
+  a : in STD_LOGIC; 
+  b : in STD_LOGIC; 
+  cin : in STD_LOGIC; 
+  s : out STD_LOGIC; 
+  cout : out STD_LOGIC); 
 end FA_dataflow; 
 architecture dataflow of FA_dataflow is 
 begin 
-s <= (a xor b) xor cin; 
-cout <= (a and b) or (b and cin) or (a and cin); 
+    s <= (a xor b) xor cin; 
+    cout <= (a and b) or (b and cin) or (a and cin); 
 end dataflow; 
