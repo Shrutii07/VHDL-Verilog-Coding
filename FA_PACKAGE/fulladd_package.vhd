ab05ca192c1df57a7d2bfa62da2da1a164b118e9
Shruti Murarka <shrutimurarka5@gmail.com>
2021-03-10 19:13:52 +0530

    Update fulladd_package.vhd

--- FA_PACKAGE/fulladd_package.vhd
+++ FA_PACKAGE/fulladd_package.vhd
@@ -2,8 +2,8 @@ LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
 
 PACKAGE fulladd_package IS
-COMPONENT fulladd
-PORT ( Cin, x, y : IN STD_LOGIC ;
+component fulladd
+port ( Cin, x, y : IN STD_LOGIC ;
 s, Cout : OUT STD_LOGIC ) ;
-END COMPONENT ;
-END fulladd_package ;
+end component;
+end fulladd_package;
