482e34df478f28d6ab4918e9581df4c7b83d29df
Shruti Murarka <shrutimurarka5@gmail.com>
2021-04-02 17:13:50 +0530

    Create FA_tb.v

---
+++ VERILOG/Full Adder/FA_tb.v
@@ -0,0 +1,56 @@
+`timescale 100ps/ 10ps
+module FA_dataflow_tb();
+reg A,B,Cin;
+wire S,Cout;
+
+FA_dataflow tb(
+.A(A),
+.B(B),
+.Cin(Cin),
+.S(S),
+.Cout(Cout)
+);
+
+
+initial
+begin
+$monitor($time, "A=%b, B=%b, Cin=%b, S=%b, Cout=%b \n", A,B,Cin,S,Cout);
+end
+
+
+initial begin
+A = 0;
+B = 0;
+Cin = 0;
+#5;
+A = 0;
+B = 0;
+Cin = 1;
+#5;
+A = 0;
+B = 1;
+Cin = 0;
+#5;
+A = 0;
+B = 1;
+Cin = 1;
+#5;
+A = 1;
+B = 0;
+Cin = 0;
+#5;
+A = 1;
+B = 0;
+Cin = 1;
+#5;
+A = 1;
+B = 1;
+Cin = 0;
+#5;
+A = 1;
+B = 1;
+Cin = 1;
+#5;
+end
+
+endmodule
