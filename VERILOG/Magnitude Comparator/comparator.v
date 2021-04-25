c7647fc198f89c68a7f584708b083d219daa5833
Shruti Murarka <shrutimurarka5@gmail.com>
2021-04-25 19:34:37 +0530

    Update comparator.v

--- VERILOG/Magnitude Comparator/comparator.v
+++ VERILOG/Magnitude Comparator/comparator.v
@@ -8,16 +8,16 @@ output e_op;
 reg L_op,g_op,e_op;
 always @ (a_in,b_in) begin
 if (a_in < b_in)
-L_op=1'b1;
+  L_op=1'b1;
 else
-L_op=1'b0;
+  L_op=1'b0;
 if (a_in > b_in)
-g_op=1'b1;
+  g_op=1'b1;
 else
-g_op=1'b0;
+  g_op=1'b0;
 if (a_in == b_in)
-e_op=1'b1;
+  e_op=1'b1;
 else
-e_op=1'b0;
+  e_op=1'b0;
 end
 endmodule
