0a8d894db86d228ff18a6a8ff4a65e50522a468b
Shruti Murarka <shrutimurarka5@gmail.com>
2021-04-25 18:50:55 +0530

    Create FS_struct.v

---
+++ VERILOG/Full Subtractor/FS_struct.v
@@ -0,0 +1,16 @@
+module fullsubtdf(diff,borrow,a,b,c); 
+
+output diff,borrow; 
+
+input a,b,c; 
+assign diff = a^b^c; ;
+wire a0,q,r,s,t;
+
+not(a0,a);
+xor(x,a,b);
+xor(diff,x,c);
+and(y,a0,b);
+and(z,~x,c);
+or(borrow,y,z);
+  
+endmodule
